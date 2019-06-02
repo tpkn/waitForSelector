2613a53cbf562de186cdc48349710b3ec01c5660
tpkn <tpkn@users.noreply.github.com>
2019-06-02 15:46:41 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # waitForSelector
 Wait for an element(s) with a specific selector to appear 
+
+
+## API
+```javascript
+waitForSelector(selector, cb[, options])
+```
+
+### selector
+**Type**: _String_ | _Array_   
+Css selector or an array of selectors
+
+
+### cb
+**Type**: _Function_   
+Callback function
+
+
+### options
+**Type**: _Object_   
+
+
+### options.delay
+**Type**: _Number_   
+**Default**: `50`   
+Recheck delay in `msec`
+
+
+### options.race
+**Type**: _Boolean_   
+**Default**: `false`   
+Wait until one of the given selectros appears in DOM
+
+
+### return
+Returns `setInterval` id   
+
+
+## Usage
+```javascript
+waitForSelector('.progress-bar__fill', () => {
+   console.log('Yey! Single!');
+})
+
+waitForSelector(['.progress-bar__fill', '.modal-menu--small'], () => {
+   console.log('Yey! Array!');
+})
+```
+
---
+++ waitForSelector.js
@@ -0,0 +1,74 @@
+/*! waitForSelector, http://tpkn.me */
+function waitForSelector(selectors, cb, options){
+   var isMultiple = Array.isArray(selectors);
+
+   if(!isMultiple && typeof selectors !== 'string'){
+      throw new TypeError('First argument must be a string or an Array!');
+   }
+
+   if(typeof cb !== 'function'){
+      throw new Error('Callback function no set!');
+   }
+
+   var i, el, len, selector, found = [];
+
+   var opt   = typeof options   !== 'object'    ? {}       : options;
+   var delay = typeof opt.delay !== 'number'    ? 50       : opt.delay;
+   var race  = typeof opt.race  !== 'undefined' ? opt.race : false;
+
+   // Filter clones from the list
+   if(isMultiple){
+      selectors = selectors.filter(function (item, i, array){
+         return array.indexOf(item) == i;
+      });
+   }
+
+   var aid = setInterval(function(){
+      // Multiple
+      if(isMultiple){
+         for(i = 0, len = selectors.length; i < len; i++){
+            selector = selectors[i];
+            
+            el = document.querySelector(selector);
+            if(el && found.indexOf(selector) == -1){
+
+               // Race complete
+               if(race){
+                  clearInterval(aid);
+                  cb(selector);
+                  break;
+               }
+
+               found.push(selector);
+            }
+
+            if(found.length == selectors.length){
+               clearInterval(aid);
+               cb(found);
+            }
+         }
+
+      // Single
+      }else{
+         el = document.querySelector(selectors);
+         if(el){
+            clearInterval(aid);
+            cb(selectors);
+         }
+      }
+   }, delay);
+
+   return aid;
+}
+
+if(typeof module !== 'undefined' && typeof module.exports !== 'undefined'){
+   module.exports = waitForSelector;
+}else{
+   if(typeof define === 'function' && define.amd){
+      define([], function(){
+         return waitForSelector;
+      });
+   }else{
+      window.waitForSelector = waitForSelector;
+   }
+}
